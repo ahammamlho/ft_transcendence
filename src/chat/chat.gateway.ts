c0c8141446b5c93561af69fabd5cad1626db4801
Lhoussaine Ahammam <lahammam@e3r1p18.1337.ma>
2023-09-28 17:02:13 +0100

    fix chat client to client

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,5 +1,6 @@
 import { Logger } from '@nestjs/common';
 import {
+  OnGatewayDisconnect,
   OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
@@ -8,17 +9,47 @@ import {
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({ namespace: '/chat' })
-export class ChatGateway implements OnGatewayInit {
+export class ChatGateway implements OnGatewayInit, OnGatewayDisconnect {
   private logger: Logger = new Logger('ChatGateway');
+  private connectedClients: Map<string, Socket> = new Map();
   @WebSocketServer() wss: Server;
 
   afterInit(server: any) {
     this.logger.log('Initialized');
   }
+  handleDisconnect(client: any) {
+    console.log(`Client disconnected: ${client.id}`);
+    for (let key of this.connectedClients.keys()) {
+      if (client.id === this.connectedClients.get(key).id) {
+        this.connectedClients.delete(key);
+        this.wss.emit('addClient', { action: 'remove', username: key });
+        break;
+      }
+    }
+  }
+
+  @SubscribeMessage('identify')
+  handleIdentify(client: Socket, userId: string): void {
+    // Associate the user ID with the WebSocket connection
+    if (userId !== '') {
+      this.connectedClients.set(userId, client);
+      for (let key of this.connectedClients.keys()) {
+        this.wss.emit('addClient', { action: 'add', username: key });
+      }
+    }
+  }
 
   @SubscribeMessage('chatToServer')
-  handleMessage(client: Socket, message: { sender: string; message: string }) {
-    console.log('I get msg from client');
-    this.wss.emit('chatToClient', message);
+  handleMessage(
+    client2: Socket,
+    message: { sender: string; sendTo: string; message: string },
+  ) {
+    const client = this.connectedClients.get(message.sendTo);
+    console.log(message);
+    if (client) {
+      client.emit('chatToClient', message);
+    } else {
+      console.log(`Client not found.`);
+    }
   }
 }
--- static/script.js
+++ static/script.js
@@ -2,24 +2,66 @@ document.addEventListener('DOMContentLoaded', () => {
   const messageForm = document.getElementById('messageForm');
   const messageInput = document.getElementById('messageInput');
   const messageList = document.getElementById('messageList');
+  const clientsList = document.getElementById('clientsList');
+  const nameUser = document.getElementById('nameUser');
 
   const username = prompt('Enter your Name');
-
+  let userToSend = '';
   const socketChat = io('http://localhost:3000/chat');
+  // make connection whith server
+  socketChat.emit('identify', username);
+  //  { action: 'add', username: key }
+  socketChat.on('addClient', (value) => {
+    if (value.action === 'add' && value.username !== '') {
+      let isExist = 0;
+      for (let i = 0; i < clientsList.children.length; i++) {
+        if (clientsList.children[i].innerHTML === value.username) {
+          isExist = 1;
+          break;
+        }
+      }
+      if (!isExist && value.username !== username) {
+        const clientsItem = document.createElement('button');
+        clientsItem.type = 'button';
+        clientsItem.className = 'my-button';
+        clientsItem.innerHTML = `${value.username}`;
+        clientsList.appendChild(clientsItem);
+        clientsItem.addEventListener('click', (event) => {
+          event.preventDefault();
+          for (let i = 0; i < clientsList.children.length; i++) {
+            clientsList.children[i].classList.remove('btn');
+          }
+          event.target.classList.add('btn');
+          userToSend = event.target.innerHTML;
+        });
+      } else {
+        nameUser.innerText = `I am ${username}`;
+      }
+    } else {
+      for (let i = 0; i < clientsList.children.length; i++) {
+        if (clientsList.children[i].innerHTML === value.username) {
+          clientsList.children[i].remove();
+          break;
+        }
+      }
+    }
+  });
+
   socketChat.on('chatToClient', (msg) => {
     const messageItem = document.createElement('li');
     messageItem.innerHTML = ` <strong>${msg.sender} : </strong> ${msg.message}`;
-    // messageItem.textContent = ` -> `;
     messageList.appendChild(messageItem);
   });
 
   messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    console.log(userToSend);
     const userMessage = messageInput.value.trim();
     // console.log('dasfkahsfjkhasd');
     if (userMessage !== '') {
       socketChat.emit('chatToServer', {
         sender: username,
+        sendTo: userToSend,
         message: userMessage,
       });
       messageInput.value = '';
