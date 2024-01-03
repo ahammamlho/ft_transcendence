b8ad50351d153b0adf69f60dd0194ebfcb4bc458
AHAMMAM LHOUSSAINE <ahammamlho96@gmail.com>
2024-01-03 14:47:59 +0100

    add dto notifica

--- backend/src/notification/notification.controller.ts
+++ backend/src/notification/notification.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, UseGuards, Param, Body, Delete, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  UseGuards,
+  Param,
+  Body,
+  Delete,
+  Get,
+  Post,
+} from '@nestjs/common';
 import { NotificationService } from './notification.service';
 import { NotFoundException } from '@nestjs/common';
 import { JwtGuard } from 'src/auth/guard';
 
-
 @Controller('notification')
 @UseGuards(JwtGuard)
 export class NotificationController {
-  constructor(private readonly notificationService: NotificationService) { }
+  constructor(private readonly notificationService: NotificationService) {}
 
   @Post('/createNotification')
   createNotification(@Body() createNotificationDto: any) {
@@ -18,19 +25,20 @@ export class NotificationController {
   async deleteNotification(@Param('notificationId') notificationId: string) {
     try {
       await this.notificationService.deleteNotification(notificationId);
-      return { statusCode: 200, message: "Notification successfully deleted" };
+      return { statusCode: 200, message: 'Notification successfully deleted' };
     } catch (error) {
       if (error instanceof NotFoundException) {
         return { statusCode: 404, message: error.message };
       } else {
-        return { statusCode: 500, message: "Internal server error" };
+        return { statusCode: 500, message: 'Internal server error' };
       }
     }
   }
 
   @Get('/getNotifications/:senderId')
   async fetchNotifications(@Param('senderId') senderId: string) {
-    const notifications = await this.notificationService.fetchNotifications(senderId);
+    const notifications =
+      await this.notificationService.fetchNotifications(senderId);
     return notifications;
   }
 
@@ -39,5 +47,4 @@ export class NotificationController {
     const notifications = await this.notificationService.clearAll(senderId);
     return notifications;
   }
-
 }
